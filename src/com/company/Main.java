c32cd04e5141caea6fd7bd6418ff5b2289806e72
KennyWagner <wagnerkenneth72@gmail.com>
2022-04-13 02:34:26 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+	    //int age
+        //Output “Please enter your age? “
+        //Input age
+        //if age >= 21
+            //Output ”You get a wristband”
+        //end If
+
+        int age = 22;
+        if (age >= 21)
+        {
+            System.out.println("You get a wristband!");
+        }
+    }
+}
